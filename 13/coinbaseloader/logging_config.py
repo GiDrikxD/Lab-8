ee29a67ccb1bd8349ae3dea02afe3d42901eb9e9
GiDrikxD <159322273+GiDrikxD@users.noreply.github.com>
2024-05-19 12:28:01 +0300

    logging_config.py

--- 13/coinbaseloader/logging_config.py
+++ 13/coinbaseloader/logging_config.py
@@ -1,15 +1,11 @@
 import logging
 
-# Налаштування логера
 logger = logging.getLogger('coinbaseloader')
 logger.setLevel(logging.DEBUG)  # Встановлення рівня логування
 
-# Створення власного форматера
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# Створення хендлера, що пише у файл
 file_handler = logging.FileHandler('coinbaseloader.log')
 file_handler.setFormatter(formatter)
 
-# Додавання хендлера до логера
 logger.addHandler(file_handler)
